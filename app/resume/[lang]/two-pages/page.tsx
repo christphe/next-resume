dfa7f6789e92fe961412fe75c6cfbb1a09405fd2
christphe <christphe@ffxmail.com>
2024-12-13 16:18:01 +0100

    remove dead code

--- app/resume/[lang]/two-pages/page.tsx
+++ app/resume/[lang]/two-pages/page.tsx
@@ -23,19 +23,6 @@ export default async function Resume(props: ResumeProps) {
 
   const imagePath = await resumeService.getImagePath();
 
-  // return (
-  //   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-  //     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-  //       <div
-  //         dangerouslySetInnerHTML={{
-  //           __html: data.about,
-  //         }}
-  //       ></div>
-  //     </main>
-  //     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-  //   </div>
-  // );
-
   return (
     <div className="page">
       <div className="">
