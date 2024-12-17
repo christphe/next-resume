cacda1fbd889d0ccb92a5637eba764a9494d9dd8
christphe <christphe@ffxmail.com>
2024-12-17 08:55:36 +0100

    put langs in cache

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,14 +1,27 @@
 export interface I18n {
   t: (key: string) => string;
 }
+interface LangResource {
+  [key: string]: string | LangResource;
+}
+const langsCache: Record<string, Record<string, LangResource>> = {};
 export async function usei18n(lang: string): Promise<I18n> {
-  const messages = await import(`../../messages/${lang}.json`);
+  if (langsCache[lang] == null) {
+    langsCache[lang] = await import(`../../messages/${lang}.json`);
+  }
+  const messages = langsCache[lang];
   return {
     t(key: string): string {
       const path = key.split(".");
-      let value = messages;
+      let value: string | LangResource = messages;
       for (const p of path) {
+        if (typeof value !== "object") {
+          throw new Error(`Invalid key: ${key}`);
+        }
         value = value[p];
+      }
+      if (typeof value !== "string") {
+        throw new Error(`Invalid key: ${key}`);
       }
       return value;
     },
